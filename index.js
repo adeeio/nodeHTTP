c5cc86e103acaea1e6d2146ebcac67a67f4a62e4
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-30 23:07:32 +0100

    task completed

---
+++ index.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+
+
+
+
+
+
+
+var server = http.createServer(function (request, response) {
+    response.setHeader("Content-type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+      fs.readFile('./index.html', 'utf-8', function(err ,data){
+          if (err) throw err;
+        response.write(data);
+        response.end();
+      });
+        
+    } else {
+  
+        fs.readFile('./404.jpg', function(err, data) {
+            if (err) throw err;
+            response.writeHead(200, {'Content-Type': 'image/jpeg'});
+            response.write(data);
+            response.end();
+        } );
+        
+    }
+
+});
+server.listen(7777);
+
